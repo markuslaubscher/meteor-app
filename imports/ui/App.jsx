96a331e2ba89d22b98e2631e1d31e9cad6a7a325
Markus Laubscher <markuslaubscher@gmail.com>
2023-04-05 15:09:30 +0200

    render sample task

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Task } from './Task.jsx';
 
 const tasks = [
   {_id: 1, text: 'First Task'},
@@ -11,7 +10,8 @@ const tasks = [
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+    </ul>
   </div>
 );
